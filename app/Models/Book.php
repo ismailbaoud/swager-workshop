0cae74aa10fd36b641d5fc0099abd60a663d85fa
ismailbaoud <ibaoud46@gmail.com>
2025-03-19 10:25:53 +0000

    ajouter la documentation schema a la model Book

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -3,7 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+/**
+ * @OA\Schema(
+ *     schema="Book",
+ *     required={"title", "author"},
+ *     @OA\Property(property="id", type="integer", example=1),
+ *     @OA\Property(property="title", type="string", example="Laravel for Beginners"),
+ *     @OA\Property(property="author", type="string", example="John Doe"),
+ *     @OA\Property(property="created_at", type="string", format="date-time"),
+ *     @OA\Property(property="updated_at", type="string", format="date-time")
+ * )
+ */
 class Book extends Model
 {
     protected $fillable = [ 
