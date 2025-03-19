290e53dd1ff9ff1ece34bc4e1b73010c1b159ed8
ismailbaoud <ibaoud46@gmail.com>
2025-03-19 10:49:09 +0000

    j'ai oublier install:api et j'ai ajouter ca mantenant

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,13 @@
 <?php
 
-use App\Http\Controllers\BookController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+
 
     Route::get('/books', [BookController::class, 'index']);
     Route::get('/books/{id}', [BookController::class, 'show']);
-    Route::post('books', [BookController::class, 'store']);
+    Route::post('/books', [BookController::class, 'store']);
     Route::put('/books/{id}', [BookController::class, 'update']);
     Route::delete('/books/{id}', [BookController::class, 'destroy']);
 
